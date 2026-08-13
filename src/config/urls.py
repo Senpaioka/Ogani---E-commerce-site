9619bf43da3a45e17972a45d4f23bca875ef3281
senpaioka@windows <abdulmannan7589@gmail.com>
2026-08-13 14:51:17 +0600

    urls.py updated

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,41 +1,30 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    # Admin Interface
     path('admin/', admin.site.urls),
-    path('', include('apps.home.urls')),
-    path('product/', include('apps.product.urls')),
-    path('cart/', include('apps.cart.urls')),
-    path('account/', include('apps.accounts.urls')),
-    path('blog/', include('apps.blog.urls')),
-    path('contact/', include('apps.contact.urls')),
-    path('payment/', include('apps.payment.urls')),
-    path('review/', include('apps.review.urls')),
-    # paypal urls
+
+    # Core Application Routes with App Namespacing
+    path('', include('apps.home.urls', namespace='home')),
+    path('products/', include('apps.product.urls', namespace='product')),
+    path('cart/', include('apps.cart.urls', namespace='cart')),
+    path('accounts/', include('apps.accounts.urls', namespace='accounts')),
+    path('blogs/', include('apps.blog.urls', namespace='blog')),
+    path('contact/', include('apps.contact.urls', namespace='contact')),
+    path('payments/', include('apps.payment.urls', namespace='payment')),
+    path('reviews/', include('apps.review.urls', namespace='review')),
+
+    # Third-Party Packages
     path('paypal/', include("paypal.standard.ipn.urls")),
 ]
 
 
-# static file settings
+# Static and Media file serving in local development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # type: ignore[arg-type]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # type: ignore[arg-type]
+
                 
